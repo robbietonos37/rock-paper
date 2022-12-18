7684c3c971ae4995d49ac315455dbb1aedf24a47
Robbie Tonos <Retonos@go.olemiss.edu>
2022-12-17 18:54:36 -0600

    Create player and com selection. Test lowercase

--- javascript.js
+++ javascript.js
@@ -1,16 +1,25 @@
-function getComputerChoice(){
+function getComputerChoice() {
     comNum = Math.floor(Math.random() * 3);
-    comChoice;
-    switch (comNum){
-    case 0:
-        comChoice = "Rock";
-        break;
-    case 1:
-        comChoice = "Paper";
-        break;
-    case 2:
-        comChoice = "Scissors";
-        break;
+    comChoice = "";
+    switch (comNum) {
+        case 0:
+            comChoice = "Rock";
+            break;
+        case 1:
+            comChoice = "Paper";
+            break;
+        case 2:
+            comChoice = "Scissors";
+            break;
     }
     return comChoice;
 }
+
+function playRound(playerSelection, comSelection) {
+    const decider = playerSelection.toLowerCase();
+    //console.log(decider);
+}
+
+const playerSelection = "roCk";
+const comSelection = getComputerChoice();
+console.log(playRound(playerSelection, comSelection));
